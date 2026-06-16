0241755be49b0d28768f92a76ab643f59ba26a9f
camara9126 <acvision01@gmail.com>
2026-06-16 13:31:47 +0100

    mise a jour

--- app/Http/Controllers/Api/UniteController.php
+++ app/Http/Controllers/Api/UniteController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Unite;
 use Illuminate\Http\Request;
 
 class UniteController extends Controller
 {
-    //
+     public function index()
+    {
+        return response()->json(
+            Unite::with('user')->get()
+        );
+    }
+
+    public function show($id)
+    {
+        return response()->json(
+            Unite::with('user')->findOrFail($id)
+        );
+    }
 }
