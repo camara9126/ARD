1804995700f54d62be426b50648cf46c794825bc
camara9126 <acvision01@gmail.com>
2026-06-17 12:17:52 +0100

    avancement projet

--- app/Http/Controllers/Api/ProduitController.php
+++ app/Http/Controllers/Api/ProduitController.php
@@ -18,7 +18,7 @@ class ProduitController extends Controller
     public function show($id)
     {
         return response()->json(
-            Produit::with('unite')->where('unite_id', $id)->findOrFail()
+            Produit::with('unite')->where('unite_id', $id)->get()
         );
     }
 }
