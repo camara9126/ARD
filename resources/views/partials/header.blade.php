833ca21bba55935715ae621518128a5f957a4dac
camara9126 <acvision01@gmail.com>
2026-08-09 12:03:45 +0000

    mise a jour

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -145,15 +145,7 @@
                                     </ul>
                                 </div>
                             </div>
-                            <!--<div class="p-15 p-b-0">
-                                <form class="form-material">
-                                    <div class="form-group form-primary">
-                                        <input type="text" name="footer-email" class="form-control">
-                                        <span class="form-bar"></span>
-                                        <label class="float-label"><i class="fa fa-search m-r-10"></i>Search Friend</label>
-                                    </div>
-                                </form>
-                            </div>-->
+                          
                             <div class="pcoded-navigation-label">Navigation</div>
                                 <ul class="pcoded-item pcoded-left-item">
                                     <li class="active">
@@ -349,16 +341,6 @@
                                             </ul>
                                         </li>
                                     </ul>     
-
-                                    <!-- <ul class="pcoded-item pcoded-left-item">
-                                        <li class="">
-                                            <a href="{{ route('produit.index') }}" class="waves-effect waves-dark">
-                                                <span class="pcoded-micon"><i class="fas fa-box"></i><b>FC</b></span>
-                                                <span class="pcoded-mtext">Produits</span>
-                                                <span class="pcoded-mcaret"></span>
-                                            </a>
-                                        </li>
-                                    </ul>-->
                                     
                                 
                                     @if($unite->categorie->slug !== 'gos')
